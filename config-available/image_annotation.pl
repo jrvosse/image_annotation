85fa05a5e6d2082950271149ee167a9d9ef6da52
Michiel Hildebrand <m.hildebrand@vu.nl>
2012-03-22 11:39:23 +0100

    do not autoload dcterms

--- config-available/image_annotation.pl
+++ config-available/image_annotation.pl
@@ -4,9 +4,9 @@
 */
 
 :- use_module(library('semweb/rdf_db')).
-:- use_module(library('semweb/rdf_library')).
 
-:- rdf_load_library(dcterms).
+% @TBD
+% requires Dublin Core and SKOS
 
 % hack namespace
 :- rdf_register_ns(oac, 'http://www.openannotation.org/ns/').
