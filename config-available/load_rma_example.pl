d8b678509b34aac546c975e917929cf932406da3
Jacco van Ossenbruggen <Jacco.van.Ossenbruggen@cwi.nl>
2012-10-19 17:40:23 +0200

    renamed example ui config file

--- config-available/load_rma_example.pl
+++ config-available/load_rma_example.pl
@@ -2,4 +2,4 @@
 
 :- use_module(library('semweb/rdf_db')).
 
-:- rdf_load(rdf('rma_annotation.ttl')). % load example config file
+:- rdf_load(rdf('annotation_ui_example.ttl')). % load example config file
