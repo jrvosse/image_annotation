c1a3408c51ac6e084a5d062a7916d3a5ade4e4ae
Jacco van Ossenbruggen <Jacco.van.Ossenbruggen@cwi.nl>
2013-09-11 14:53:52 +0200

    clean up no longer used namespaces

--- config-available/image_annotation.pl
+++ config-available/image_annotation.pl
@@ -6,11 +6,6 @@
 :- use_module(library('semweb/rdf_db')).
 :- use_module(library('semweb/rdf_library')).
 
-% hack namespace
-% :- rdf_register_ns(oac, 'http://www.openannotation.org/ns/').
-% :- rdf_register_ns(an, 'http://semanticweb.cs.vu.nl/annotate/').
-% :- rdf_register_ns(prov, 'http://www.w3.org/ns/prov-o/').
-
 :- rdf_attach_library(image_annotation(rdf)).
 :- rdf_load_library('annotation-ui-schema').
 
