a1ceed4f189d692aef1a3a86392f2eae99697ad7
Jacco van Ossenbruggen <Jacco.van.Ossenbruggen@cwi.nl>
2014-04-29 11:00:04 +0200

    fix broken cancel/done button logic

--- web/js/deniche-editor.js
+++ web/js/deniche-editor.js
@@ -1,21 +1,36 @@
 annotorious.plugin.DenichePlugin.prototype._extendEditor = function(annotator) {
+	var oSelf = this;
 	var fieldsEl = document.getElementById('fields');
+
+	this.saveButton   = document.getElementsByClassName('annotorious-editor-button-save').item(0); 
+	this.cancelButton = document.getElementsByClassName('annotorious-editor-button-cancel').item(0); 
+
 	annotator.editor.addField(fieldsEl);
+	this.saveButton.innerHTML = "Done";
 
 	anno.addHandler('onSelectionCompleted', function(ev) {
 		// HACK:
-		// set shape when new selection is completed so we can use it in the YUI autocompletion plugins.
+		// set shape when new selection is completed 
+		// so we can use it in the YUI autocompletion plugins.
 		anno.currentShape = ev.shape;
+
 	});
 
 	anno.addHandler('onEditorShown', function(annotation) {
 		// HACK:
-		// set shape when adding to existing annotation so we can use it in the YUI autocompletion plugins.
+		// set shape when adding to existing annotation 
+		// so we can use it in the YUI autocompletion plugins.
 		if (annotation && annotation.shapes) {
 			anno.currentShape = annotation.shapes[0];
+			oSelf.cancelButton.style.display="none";
+			oSelf.saveButton.style.display="inline-block";
+		} else {
+			oSelf.saveButton.style.display="none";
+			oSelf.cancelButton.style.display="inline-block";
 		}
+
 		YUI().use('node', 'event', function(Y) {
-			Y.one('a.annotorious-editor-button-save').setHTML('Done'); // Hack: turn save into done button
+			// Y.one('a.annotorious-editor-button-save').setHTML('Done'); // Hack: turn save into done button
 			Y.all('li.tagitem').each(function(tagNode) {
 				if (annotation && annotation.targetId == tagNode.getAttribute('targetId'))
 					tagNode.show();
