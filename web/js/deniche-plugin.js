1ae7ef9626d1e0c58dc4be2944919d3d450133ce
Jacco van Ossenbruggen <Jacco.van.Ossenbruggen@cwi.nl>
2014-05-07 08:58:18 +0200

    added comment info

--- web/js/deniche-plugin.js
+++ web/js/deniche-plugin.js
@@ -1,10 +1,28 @@
+/* Annotorious plugin to use the annotation editor from the
+ * ClioPatria image_annotation cpack package as the editor for
+ * shapes created in Annotorious.
+ *
+ * To this, we need to communicate the shape that the user
+ * created in annotorious to all annotation field objects of the cpack.
+ *
+ * Note that while the annotorious editor is open, the user can add
+ * and remove tags using the cpack user interface. But we can only
+ * add/rm tags using the annotorious API when the editor is closed.
+ *
+ * As a result, we need to do some bookkeeping ourselves in the 
+ * _dirtytags array, and update annotorious when the editor is closed
+ * by flushing out all changes made.
+ *
+ * */
 
 annotorious.plugin.DenichePlugin = function(opt_config_options) { 
+	/** @public **/
+	this.currentShape = null; // Should be accessible by cpack objects
+
 	/** @private **/
-	this._cleantags = [];
-	this._dirtytags = [];
-	this.currentShape = null;
-	this.currentTargetId = null;
+	this._cleantags = [];	// tags annotorious already knows about
+	this._dirtytags = [];	// tags annotorious doesn't know yet
+	this._currentTargetId = null; // current target url so we can fin
 }
 
 annotorious.plugin.DenichePlugin.states = { EMPTY:'empty', SOME:'some' };
@@ -39,15 +57,12 @@ annotorious.plugin.DenichePlugin.prototype.filterTags = function(annotation) {
 				tagNode.hide();
 			}
 		});
-		if (zero_tags_show) {
-			oSelf.toggleButtons(annotorious.plugin.DenichePlugin.states.EMPTY);
-		}
 	});
 }
 
 annotorious.plugin.DenichePlugin.prototype.removeAnnotation = function (label, targetId) {
 	// console.log('DenichePlugin.removeAnnotation');
-	this.currentTargetId = targetId;
+	this._currentTargetId = targetId;
 	var old = this._dirtytags[targetId];
 	if (!old) old = this._cleantags[targetId];
 	if (old) {	
@@ -56,18 +71,18 @@ annotorious.plugin.DenichePlugin.prototype.removeAnnotation = function (label, t
 		annotation.compound_text = old.compound_text.splice(index, 1);
 		annotation.text = old.compound_text.join(', ');
 		this._dirtytags[targetId] = annotation;
-		if (!annotation.text) this.toggleButtons(annotorious.plugin.DenichePlugin.states.EMPTY);
 	}
 	
 }
 
 annotorious.plugin.DenichePlugin.prototype.addAnnotation = function (annotation, update) {
 	// console.log('DenichePlugin.addAnnotation', update);
-	this.currentTargetId = annotation.targetId;
+	this._currentTargetId = annotation.targetId;
 	this.toggleButtons(annotorious.plugin.DenichePlugin.states.SOME);
 	var old = this._dirtytags[annotation.targetId];
 	if (!old) old = this._cleantags[annotation.targetId];
 	if (old) {	
+		// extend new annotation by merging in old one
 		annotation.compound_text = old.compound_text;
 		annotation.compound_text.push(annotation.text);
 		annotation.text = annotation.compound_text.join(', ');
@@ -84,13 +99,16 @@ annotorious.plugin.DenichePlugin.prototype.addAnnotation = function (annotation,
 }
 
 annotorious.plugin.DenichePlugin.prototype.flushDirtyAnnotation = function(original) {
-		var dirty = this._dirtytags[this.currentTargetId];
+		var dirty = this._dirtytags[this._currentTargetId];
 		// console.log('replacing ', original, dirty);
-		if (dirty) { 
+		if (dirty.text) { 
 			this._anno.addAnnotation(dirty,original);
-			this._cleantags[this.currentTargetId] = dirty;
-			this._dirtytags[this.currentTargetId] = null;
+			this._cleantags[this._currentTargetId] = dirty;
+		} else {
+			this._anno.removeAnnotation(original);
+			this._cleantags[this._currentTargetId] = null;
 		}
+		this._dirtytags[this._currentTargetId] = null;
 }
 
 annotorious.plugin.DenichePlugin.prototype.onInitAnnotator = function(annotator) {
@@ -132,4 +150,3 @@ anno.addPlugin('DenichePlugin', {});
 // Debug:
 // anno.addHandler('beforeAnnotationRemoved', function(ev) { console.log('beforeAnnotationRemoved', ev) });
 // anno.addHandler('onAnnotationRemoved', function(ev) { console.log('onAnnotationRemoved', ev) });
-// anno.addHandler('onAnnotationUpdated', function(ev) { console.log('onAnnotationUpdated', ev) });
