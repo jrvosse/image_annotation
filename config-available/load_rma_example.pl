a6fb7567a857cf7e22e60ac3aa939b0354549f39
Chris Dijkshoorn <c.r.dijkshoorn@vu.nl>
2014-09-11 13:03:47 +0200

    Added simple bird annotation ui.

--- config-available/load_rma_example.pl
+++ config-available/load_rma_example.pl
@@ -3,3 +3,4 @@
 :- use_module(library('semweb/rdf_db')).
 
 :- rdf_load(rdf('annotation_ui_example.ttl')). % load example config file
+:- rdf_load(rdf('annotation_ui_bird.ttl')). % load bird config file
