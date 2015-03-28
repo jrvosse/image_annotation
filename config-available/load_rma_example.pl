c9b3e480cf46c842836ca6683a4d9e39006b78ea
Chris Dijkshoorn <c.r.dijkshoorn@vu.nl>
2015-03-28 17:22:17 +0000

    Updated bible and bird annotation uis and loaded bible in code.

--- config-available/load_rma_example.pl
+++ config-available/load_rma_example.pl
@@ -4,3 +4,4 @@
 
 :- rdf_load(rdf('annotation_ui_example.ttl')). % load example config file
 :- rdf_load(rdf('annotation_ui_bird.ttl')). % load bird config file
+:- rdf_load(rdf('annotation_ui_bible.ttl')).
